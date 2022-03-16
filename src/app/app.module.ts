a9d57673eb6b098025e5275da20c7acdd5210c5a
Dev Web Junior <50543732+DevWebJr@users.noreply.github.com>
2022-03-16 22:47:30 +0100

    add partial's elements like header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { DeviceComponent } from './device/device.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeviceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
